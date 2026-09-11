cccd6336e0d6b2f52637689910a3507e207b4950
ltv-Jindo <jindodino123@gmail.com>
2026-09-11 20:41:02 +0700

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,10 +1,10 @@
 $folder = "$env:TEMP\JindoMultiTool"
-$bat = "$folder\main.bat"
+$bat = "$folder\main_webver.bat"
 
 New-Item -ItemType Directory -Path $folder -Force | Out-Null
 
 Invoke-WebRequest `
-    -Uri "https://ltv-jindo.github.io/host/main.bat" `
+    -Uri "https://ltv-jindo.github.io/host/main_webver.bat" `
     -OutFile $bat
 
 Start-Process "cmd.exe" -ArgumentList "/c `"$bat`""
